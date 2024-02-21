cd5f13ed350592528f0bb323e603e6b3284a8e6e
Nilesh <nileshparab5623@gmail.com>
2024-02-21 17:13:44 +0530

    Data Validation

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -2,9 +2,18 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
